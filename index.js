0bf4bc0f9f5d93fb6637051d98f0e05bb2fbdee8
Robert Culbert <rfculbert@gmail.com>
2022-11-16 15:47:51 -0600

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-console.log('hello world')
+// console.log('hello world')
+const message = document.querySelector('#message')
+document.querySelector("form").addEventListener("submit", addMovie)
 
-const addMovie = (event) => {
+function addMovie (event)  {
+    event.preventDefault()
     let inputField = document.querySelector('input')
-    let movie = document.createElement(li)
-    let movieTitle = document.createElement("span")
+
+
+    const movie = document.createElement('li')
+
+    const movieTitle = document.createElement("span")
     movieTitle.textContenet = inputField.value;
     movie.appendChild(movieTitle)
     
-    let movie = document.querySelector('ul')
+    
+    const deleteBtn =document.createElement("button")
+    deleteBtn.textContent = "X"
+    deleteBtn.addEventListener('click', deleteMovie)
+    movieTitle.addEventListener('click', crossOffMovie)
+    movie.appendChild(deleteBtn)
+    
+    const list = document.querySelector('ul')
+    list.appendChild(movie)
+    
+    
+    inputField.vaule = ''
 }
 
-document.querySelector('form').addEventListener('submit', addMovie)
+
+function deleteMovie(event) {
+    event.target.parentNode.remove()
+    message.textContent = "Movie deleted!"
+}
+
+
+function crossOffMovie(event) {
+    event.target.classList.toggle('checked')
+    if (event.target.classList.contains('checked') === true){
+        message.textContent = "Movie Watched!"
+    }else {
+        message.textContent = "Movie Added Back!"
+    }
+}
