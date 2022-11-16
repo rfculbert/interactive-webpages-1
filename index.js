86779a6709ee30b034bce8240f63664e5930b658
Robert Culbert <rfculbert@gmail.com>
2022-11-16 16:04:12 -0600

    final commit

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function addMovie (event)  {
 
     const movieTitle = document.createElement("span")
     movieTitle.textContenet = inputField.value;
+    movieTitle.addEventListener("click", crossOffMovie)
     movie.appendChild(movieTitle)
     
     
     const deleteBtn =document.createElement("button")
     deleteBtn.textContent = "X"
     deleteBtn.addEventListener('click', deleteMovie)
-    movieTitle.addEventListener('click', crossOffMovie)
     movie.appendChild(deleteBtn)
     
     const list = document.querySelector('ul')
@@ -31,14 +31,23 @@ function addMovie (event)  {
 function deleteMovie(event) {
     event.target.parentNode.remove()
     message.textContent = "Movie deleted!"
+    revealMessage()
 }
 
 
 function crossOffMovie(event) {
     event.target.classList.toggle('checked')
     if (event.target.classList.contains('checked') === true){
-        message.textContent = "Movie Watched!"
+        message.textContent = `${event.target.textContenet} watched!`
     }else {
         message.textContent = "Movie Added Back!"
     }
+    revealMessage()
+}
+
+function revealMessage() {
+
+    setTimeout(() => {
+        message.classList.add("hide")
+    }, 1000)
 }
